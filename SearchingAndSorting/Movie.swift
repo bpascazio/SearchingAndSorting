17228b5b4885600a394f5722995bcbd6cf82a602
Bob Pascazio <bob@bytefly.com>
2015-12-21 20:03:00 -0500

    Updates?

---
+++ SearchingAndSorting/Movie.swift
@@ -0,0 +1,22 @@
+//
+//  Movie.swift
+//  SearchingAndSorting
+//
+//  Created by Bob Pascazio on 12/21/15.
+//  Copyright © 2015 Bytefly, Inc. All rights reserved.
+//
+
+import Foundation
+
+class Movie {
+    
+    var title:String
+    var grossing:Float
+    
+    init(titleValue:String, grossingValue:Float) {
+        
+        title = titleValue
+        grossing = grossingValue
+    }
+    
+}
